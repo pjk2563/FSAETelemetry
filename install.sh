3ae99e9ff1ab3b209114c7dcab23c8e6757dec9f
Evan M <gatortech12@gmail.com>
2018-09-27 21:40:15 -0400

    Got most of the script written. Will run tests tomorrow.

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 echo This is the install script that will handle the installation of the telemetery system on reciever pi
+su -
+apt-get update && apt-get full-upgrade -y
+apt-get install curl
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+test $VERSION_ID = "9" && echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+apt-get update && apt-get upgrade -y
+apt-get install influxdb -y
+service influxdb start
