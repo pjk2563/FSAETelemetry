e4220a9d50cfb89e28c4a93906d44c1d793882a1
Evan <ewm2000@users.noreply.github.com>
2018-10-16 21:48:24 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-echo This is the install script that will handle the installation of the telemetery system on reciever pi
-apt-get update && apt-get full-upgrade -y
-apt-get install curl -y
-curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-test $VERSION_ID = "9" && echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-apt-get update && apt-get upgrade -y
-apt-get install influxdb -y
-service influxdb start
-#Will add the grafana install instructions when I have my hands on a pi
