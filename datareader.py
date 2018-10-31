48f285c72f4f39fc909e1cce41cc8286ce7e206d
Jensen Li <jensenli049@gmail.com>
2018-10-30 21:38:39 -0400

    rearranged data and reconfigured reader

--- datareader.py
+++ datareader.py
@@ -10,13 +10,13 @@ from influxdb import InfluxDBClient
 
 dictionary = {}
 #make 2d list
-labls = ['rpm', 'volt', 'gear', 'tmp', 'spd', 'gfs']
+labls = ['time','gear','nmot','poil','speed','tint','tmot','toil','ub']
 for val in labls:
     dictionary[val] = []
 
 def main():
     getData("telemetry-data.csv")
-    print(str(dictionary['rpm']))
+    print(str(dictionary['nmot']))
     """
     host='192.168.43.1'
     port=3000
@@ -29,17 +29,23 @@ def main():
     client.write_points([dictionary])"""
 
 def getData(filename):
+    time = 0.0
     file=open(filename, 'r')
     #add data to dictionary according to label
     for line in file:
         temp = line.split(",")
-        if(temp[0] == "ECU_IDprim"):
-            dictionary[labls[0]].append(float(temp[1]))
-            dictionary[labls[1]].append(float(temp[2]))
-            dictionary[labls[2]].append(float(temp[3]))
-            dictionary[labls[3]].append(float(temp[4]))
-            dictionary[labls[4]].append(float(temp[5]))
-            dictionary[labls[5]].append(float(temp[6]))
+        if(temp[0] == "wait"):
+            time += float(temp[1])
+        else:
+            dictionary[labls[0]].append(time)
+            dictionary[labls[1]].append(float(temp[0]))
+            dictionary[labls[2]].append(float(temp[1]))
+            dictionary[labls[3]].append(float(temp[2]))
+            dictionary[labls[4]].append(float(temp[3]))
+            dictionary[labls[5]].append(float(temp[4]))
+            dictionary[labls[6]].append(float(temp[5]))
+            dictionary[labls[7]].append(float(temp[6]))
+            dictionary[labls[8]].append(float(temp[7]))
         
 if __name__ == '__main__': 
     main()
--- xlsxtocsv.py
+++ xlsxtocsv.py
@@ -13,21 +13,18 @@ def main():
         r = len(data['A'])
         lastprog = -1
         for i in range(2,r):
-            prog = int((i / r) * 100);
+            prog = int((i / r) * 100)
             if prog != lastprog:
                 print(str(prog) + "%", end = "\r")
                 lastprog = prog
-            csv.write("ECU_IDprim,"
+            csv.write(data["B" + str(i)].value.strip() + ","
             + data["C" + str(i)].value.strip() + ","
-            + data["H" + str(i)].value.strip() + ","
-            + data["G" + str(i)].value.strip() + ","
             + data["D" + str(i)].value.strip() + ","
-            + data["B" + str(i)].value.strip() + ","
-            + data["E" + str(i)].value.strip() + "\n")
-
-            csv.write("ECU_IDsec,"
-            + data["I" + str(i)].value.strip() + ","
-            + data["F" + str(i)].value.strip() + "\n")
+            + data["E" + str(i)].value.strip() + ","
+            + data["F" + str(i)].value.strip() + ","
+            + data["G" + str(i)].value.strip() + ","
+            + data["H" + str(i)].value.strip() + ","
+            + data["I" + str(i)].value.strip() + "\n")
 
             time_stamp = float(data["A" + str(i+1)].value) - float(data["A" + str(i)].value)
             csv.write("wait," + str(time_stamp) + "\n")
