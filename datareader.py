1c755947b3ad625e01bd6c637dc40e6ce730742b
Jensen Li <jensenli049@gmail.com>
2018-10-30 20:44:02 -0400

    changed the read format

--- datareader.py
+++ datareader.py
@@ -4,6 +4,7 @@ jl3560@rit.edu
 9/25/18
 
 Reads certain formatted data and places it into influx database
+
 """
 from influxdb import InfluxDBClient
 
@@ -16,6 +17,7 @@ for val in labls:
 def main():
     getData("random.dat")
     #print(dictionary['rpm'])
+    """
     host='192.168.43.1'
     port=3000
     user = 'admin'
@@ -24,17 +26,19 @@ def main():
 
     client = InfluxDBClient(host, port, user, password, dbname)
     client.create_database(dbname)
-    client.write_points([dictionary])
+    client.write_points([dictionary])"""
 
 def getData(filename):
     file=open(filename, 'r')
     #add data to dictionary according to label
     for line in file:
         temp = line.split(",")
-        if temp[0] in dictionary.keys():
-            dictionary[temp[0]].append(float(temp[1]))
-        else:
-            dictionary[temp[0]] = float(temp[1])
+        dictionary[labls[0]].append(float(temp[0]))
+        dictionary[labls[1]].append(float(temp[1]))
+        dictionary[labls[2]].append(float(temp[2]))
+        dictionary[labls[3]].append(float(temp[3]))
+        dictionary[labls[4]].append(float(temp[4]))
+        dictionary[labls[5]].append(float(temp[5]))
 
 if __name__ == '__main__': 
     main()
