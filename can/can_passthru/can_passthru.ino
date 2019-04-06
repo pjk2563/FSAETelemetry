a285ed3296fe9a582aec27f78d54a0d17d857338
root <root@localhost.localdomain>
2019-04-06 12:26:10 -0400

    switched to can

---
+++ can/can_passthru/can_passthru.ino
@@ -0,0 +1,55 @@
+// CAN-BUS Shield, receive data with check mode 
+// for use in remote telemetry
+// Paul Kelly, pjk2563@rit.edu, 4/4/19
+
+#include <SPI.h>
+#include "mcp_can.h"
+
+#define SER_BAUD 115200
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+void setup()
+{
+    Serial.begin(SER_BAUD);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
+}
+
+
+void loop()
+{
+    unsigned char len = 0;
+    unsigned char buf[8];
+
+    if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
+    {
+        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+        unsigned long canId = CAN.getCanId();
+
+        Serial.print("0x");
+        Serial.println(canId, HEX);
+
+        for(int i = 0; i<len; i++)    // print the data
+        {
+            Serial.print(buf[i], HEX);
+            Serial.print("\t");
+        }
+        Serial.println();
+    }
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
